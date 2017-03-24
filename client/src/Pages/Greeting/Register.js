6ddda67135010ae2e0064443c9deb35182c2f383
plusgut <github@webentwickler2-0.de>
2017-03-24 22:38:12 +0100

    [register] options are now more flexible

--- client/src/Pages/Greeting/Register.js
+++ client/src/Pages/Greeting/Register.js
@@ -3,26 +3,49 @@ import React, { Component } from 'react';
 class Register extends Component {
   constructor(props) {
     super(props);
+    var gender = [
+      'Mann',
+      'Frau',
+      'Sonstiges'
+    ];
+    
     this.state = {
+      user: {},
       infos: [{
+        label: 'Username',
+        key: 'myself',
+        input: this.inputText.bind(this),
+        option: gender,
+      }, {
+        label: 'Ich bin',
+        key: 'myself',
+        input: this.optioninfo.bind(this),
+        options: gender,
+      }, {
         label: 'Ich suche',
         key: 'search',
-        options: [
-          'Mann',
-          'Frau',
-          'Sonstiges'
-        ],
-        value: ''
+        input: this.optioninfo.bind(this),
+        options: gender,
       }]
     }
+  }
+
+  echo(value) {
+    return value;
+  }
+
+  setFromEvent(key, evt) {
+    this.setValue(key, evt.target.value);
   }
 
   submit() {
 
   }
 
-  setValue(infoIndex) {
-
+  setValue(key, value) {
+    this.setState((state) => {
+      state.user[key] = value;
+    });
   }
 
   render() {
@@ -35,13 +58,27 @@ class Register extends Component {
               {info.label}
             </div>
             <div className="col-xs-6">
-              {info.options.map((option, optionIndex) => 
-                <div>{option}</div>
-              )}
+              {info.input(info)}
             </div>
           </div>
         )}
       </form>
+    );
+  }
+
+  inputText(info) {
+    return (
+      <input type="text" onChange={this.setFromEvent.bind(this, info.key)} />
+    );
+  }
+
+  optioninfo(info) {
+    return (
+      <div>
+        {info.options.map((option, optionIndex) => 
+          <div onClick={this.setValue.bind(this, info.key, option)} >{option}</div>
+        )}
+      </div>
     );
   }
 }
