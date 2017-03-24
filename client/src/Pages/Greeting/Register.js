870922b15951b62190988892fa1cc7bf26a636b8
plusgut <github@webentwickler2-0.de>
2017-03-24 22:16:25 +0100

    [register] added some configurationable stuff

--- client/src/Pages/Greeting/Register.js
+++ client/src/Pages/Greeting/Register.js
@@ -1,9 +1,47 @@
 import React, { Component } from 'react';
 
 class Register extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      infos: [{
+        label: 'Ich suche',
+        key: 'search',
+        options: [
+          'Mann',
+          'Frau',
+          'Sonstiges'
+        ],
+        value: ''
+      }]
+    }
+  }
+
+  submit() {
+
+  }
+
+  setValue(infoIndex) {
+
+  }
+
   render() {
     return (
-      <span>Register!</span>
+      <form onSubmit={this.submit.bind(this)}>
+
+        {this.state.infos.map((info, infoIndex) =>
+          <div className="row">
+            <div className="col-xs-6">
+              {info.label}
+            </div>
+            <div className="col-xs-6">
+              {info.options.map((option, optionIndex) => 
+                <div>{option}</div>
+              )}
+            </div>
+          </div>
+        )}
+      </form>
     );
   }
 }
