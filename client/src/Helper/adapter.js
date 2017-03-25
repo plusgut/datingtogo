f873e5e039df651c8951cb4d1ee4ccd8e7d26066
plusgut <github@webentwickler2-0.de>
2017-03-25 04:09:02 +0100

    [users] actually randomized

--- client/src/Helper/adapter.js
+++ client/src/Helper/adapter.js
@@ -1,19 +1,31 @@
 var url = 'http://localhost/';
 import debug from '../Config/debug';
 
+const USER_COUNT = 3;
+const RANDOMIZE = 5;
 function mock(type) {
   var position = [11.6222117, 48.150102];
+
+  var users = [];
+  for(var i = 0; i < USER_COUNT; i++) {
+    users.push({
+      username: 'plusgut_' + i,
+      position: [randomize(position[0], RANDOMIZE), randomize(position[1], RANDOMIZE)]
+    });
+  }
   return {
     register: {
       ack: true,
     },
-    users: [
-      {
-        username: 'plusgut',
-        position: position
-      }
-    ]
+    users: users
   }[type];
+}
+
+function randomize(value, length) {
+  var stringValue = value + '';
+  value = stringValue.slice(0, stringValue.length - length) + (Math.random() + '').slice(2, 2 + length);
+  
+  return parseFloat(value, 10);
 }
 
 function get(method, params) {
